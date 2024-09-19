5b507a71ec9deec13942792e8a685d9d5bca51b8
abhishek singh <singhsabhishek23@gmail.com>
2024-09-19 13:19:21 +0530

    multipal route handler

--- devTenderBackend/src/app.js
+++ devTenderBackend/src/app.js
@@ -3,21 +3,34 @@ const express = require('express')
 const app = express()
 
 
-app.get('/user' , (req ,res)=>{
-    console.log(req.query)
-    res.send({firstName : 'abhishek' , lastName : 'yadav'})
-})
-
-// app.post('/user' , (req ,res)=>{
-   
-//    res.send("data successfully savsed to database")
+// app.get('/user' , (req ,res)=>{
+//     console.log(req.query)
+//     res.send({firstName : 'abhishek' , lastName : 'yadav'})
 // })
 
-// app.delete('/delete' , (req ,res)=>{
-   
-//     res.send("data delete savsed to database")
-//  })
- 
+app.use('/user' , (req , res ,next)=>{
+    console.log('handling the routes user 1')
+    // res.send('response 1 ')
+    next()
+}
+,
+(req , res ,next)=>{
+    console.log('handling the routes user 2')
+    // res.send('response 2 ')
+    next()
+},
+(req , res ,next)=>{
+    console.log('handling the routes user 3')
+    // res.send('response 3 ')
+    next()
+},
+(req , res ,next)=>{
+    console.log('handling the routes user 4')
+    res.send('response 4 ')
+}
+)
+
+
 
 
 app.listen(3000 , ()=>{
