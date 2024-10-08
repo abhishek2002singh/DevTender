da4be62358118cc03334456e7f66ed71fd3a88ff
abhishek singh <singhsabhishek23@gmail.com>
2024-10-08 12:50:00 +0530

    same changes

--- devTenderBackend/src/app.js
+++ devTenderBackend/src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 //add middleware from express
 app.use(corse({
-    origin: "http://localhost:5173",
+    origin: "https://fluffy-pudding-ed4ebf.netlify.app/",
     credentials:true,
 }));
 app.use(express.json())
