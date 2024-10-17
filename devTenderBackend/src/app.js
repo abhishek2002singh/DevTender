d84504fee27dbad34f23afe2cad5a97b088135c1
abhishek singh <singhsabhishek23@gmail.com>
2024-10-17 18:06:05 +0530

    changes durind deploy

--- devTenderBackend/src/app.js
+++ devTenderBackend/src/app.js
@@ -3,18 +3,19 @@ require("dotenv").config();
 const app = express();
 const dbConnect = require('./config/database');
 const cookieParser = require('cookie-parser')
-const corse = require('cors')
+
 const port = process.env.PORT || 3000;
 const path = require('path')
 
 const _dirname = path.resolve();
 
-
-//add middleware from express
-app.use(corse({
-    origin: "http://localhost:5173",
-    credentials:true,
-}));
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+  
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(cookieParser())
 
@@ -32,7 +33,7 @@ app.use('/' ,requestRouter)
 app.use('/',userRouter)
 
 
-app.use(express.static(path.join(_dirname , "\DevTenderFrontend/dist")))
+app.use(express.static(path.join(_dirname , "/DevTenderFrontend/dist")))
 
 app.get('*',(req , res)=>{
     res.sendFile(path.resolve(_dirname ,"DevTenderFrontend" , "dist" ,"index.html" ))
